62041a4a29c5e122e4ce698e38de7a1b591b3b96
HowdyWorId <69632436+HowdyWorId@users.noreply.github.com>
2021-10-01 18:41:03 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,27 +1,10 @@
 import socket
+import time
 
 sock = socket.socket(family=socket.AF_INET,
-                     type=socket.SOCK_STREAM)
-# sock.bind(("192.168.1.101", 8082))
-sock.bind(("94.41.17.116", 8082))
-print('Server started!')
-class Data:
-    data = []
-    def __new__(cls, *args, **kwargs):
-        return cls.data.append(args[0])
-
-# sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.listen()
-while 1:
-    conn, addr = sock.accept()
-    print(addr)
-    data = []
-    while 1:
-        _data = conn.recvfrom(2)
-        if _data == b'':
-            break
-        print(_data)
-        Data(_data) # just test ;)
-        # data.append(_data)
-    # print(data)
-    print(Data.data)
+              type=socket.SOCK_STREAM)
+sock.connect(("192.168.1.101", 8082))
+print('connected!')
+while True:
+    msg = input('Message\n')
+    sock.send(msg.encode('utf-8'))
